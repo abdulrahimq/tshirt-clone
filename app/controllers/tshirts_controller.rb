402060318123ab83fed372b7c45cca5379f53e9c
Abed Qaddoumi <abdulrahim.qaddoumi@gmail.com>
2019-03-18 16:37:54 -0600

    added the index, new, create and tshirt_params functionality

--- app/controllers/tshirts_controller.rb
+++ app/controllers/tshirts_controller.rb
@@ -1,10 +1,22 @@
 class TshirtsController < ApplicationController
   def index
+    @tshirts = Tshirt.all
   end
 
   def new
+    @tshirt = Tshirt.new
   end
 
   def create
+    @tshirt = Tshirt.new(tshirt_params)
+    @tshirt.save
+
+    redirect_to tshirt_path(@tshirt)
+  end
+
+  private
+
+  def tshirt_params
+    params.require(:tshirt).permit(:name, :description)
   end
 end
