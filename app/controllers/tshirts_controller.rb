655682a36492e1d64a6d1e1aa3ed2914d36d8a91
Abed Qaddoumi <abdulrahim.qaddoumi@gmail.com>
2019-03-18 16:40:42 -0600

    fixed the path of the tshirts

--- app/controllers/tshirts_controller.rb
+++ app/controllers/tshirts_controller.rb
@@ -11,7 +11,7 @@ class TshirtsController < ApplicationController
     @tshirt = Tshirt.new(tshirt_params)
     @tshirt.save
 
-    redirect_to tshirt_path(@tshirt)
+    redirect_to tshirts_index_path
   end
 
   private
