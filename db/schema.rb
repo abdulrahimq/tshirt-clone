30ced536489ba7ec65aa5f1f3e7f952baafe187f
Abed Qaddoumi <abdulrahim.qaddoumi@gmail.com>
2019-03-18 16:25:57 -0600

    added rentals

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_03_18_213831) do
-
-
-
+ActiveRecord::Schema.define(version: 2019_03_18_222524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "rentals", force: :cascade do |t|
+    t.string "content"
+    t.string "text"
+    t.bigint "user_id"
+    t.bigint "tshirt_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tshirt_id"], name: "index_rentals_on_tshirt_id"
+    t.index ["user_id"], name: "index_rentals_on_user_id"
+  end
+
+  create_table "tshirts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "description"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -33,12 +47,7 @@ ActiveRecord::Schema.define(version: 2019_03_18_213831) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-  
-  create_table "tshirts", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "name"
-    t.string "description"
-  end
 
+  add_foreign_key "rentals", "tshirts"
+  add_foreign_key "rentals", "users"
 end
