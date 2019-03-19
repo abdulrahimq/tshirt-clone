7505390efbafaf52710e93d50f14e86d6c661056
Zachary Irlbeck <zachCDMX@gmail.com>
2019-03-19 12:04:27 -0600

    started adding an admin view of all users and a show page for the users in additon to devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,9 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
-  before_action :find_user
   def index
     @users = User.all
   end
 
   def show
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  private
-
-  def find_user
-    @user = User.find(params[:id])
+    @user = User.find(current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'users/index'
-  get 'users/show'
-  resources :users
+
   resources :tshirts, only: [:index, :new, :create]
   devise_for :users
+  resources :users, only: [:index, :show]
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
