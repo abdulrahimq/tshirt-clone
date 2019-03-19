a9f4b15897b4a4f8254377e1bc22d3b44dc219e8
Zachary Irlbeck <zachCDMX@gmail.com>
2019-03-19 16:26:29 -0600

    allows users to upload photo as part of the profile edit form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_19_190425) do
+ActiveRecord::Schema.define(version: 2019_03_19_220628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,9 @@ ActiveRecord::Schema.define(version: 2019_03_19_190425) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "description"
+    t.bigint "user_id"
     t.string "photo"
+    t.index ["user_id"], name: "index_tshirts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,10 +48,12 @@ ActiveRecord::Schema.define(version: 2019_03_19_190425) do
     t.string "last_name"
     t.string "type_of"
     t.boolean "admin"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "rentals", "tshirts"
   add_foreign_key "rentals", "users"
+  add_foreign_key "tshirts", "users"
 end
