a72ff66c3459dbd88eaae26929e3d433ef5044f5
Zachary Irlbeck <zachCDMX@gmail.com>
2019-03-20 10:52:04 -0600

    finally got the user photo to work

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :tshirts
 
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
   root to: 'pages#home'
   resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
