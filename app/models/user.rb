e00cfe07ec994e81723839b40f7cd104266fde2d
Zachary Irlbeck <zachCDMX@gmail.com>
2019-03-25 16:53:11 -0600

    now when you delete a user, you delete the tshirts as well

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :tshirts
+  has_many :tshirts, dependent: :destroy
   has_many :rentals
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
