261bcddf082fe1ebaffaddc9b3eb6b35c4ba33bb
Zachary Irlbeck <zachCDMX@gmail.com>
2019-03-25 16:45:15 -0600

    modified devise controller to overide redirect on edit

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,13 +2,17 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
 
-private
+  private
 
- def sign_up_params
-  params.require(:user).permit(:photo, :first_name, :last_name, :email, :password, :password_confirmation)
- end
+  def after_update_path_for(resource)
+    user_path(resource)
+  end
 
- def account_update_params
-   params.require(:user).permit(:photo, :first_name, :last_name, :email, :password, :password_confirmation, :current_password)
- end
+  def sign_up_params
+    params.require(:user).permit(:photo, :first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:photo, :first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+  end
 end
