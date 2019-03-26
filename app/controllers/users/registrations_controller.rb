79ad9169cbcd492ad8a0a62345312e598fe10c71
Abed Qaddoumi <abdulrahim.qaddoumi@gmail.com>
2019-03-26 12:46:03 -0600

    removed after_update_path_for"

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,11 +2,6 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   private
-  # After_update_path_for isn't needed probably
-  def after_update_path_for(resource)
-    user_path(resource)
-  end
-
   def sign_up_params
     params.require(:user).permit(:photo, :first_name, :last_name, :email, :password, :password_confirmation)
   end
