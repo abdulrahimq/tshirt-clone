d21ac3561acfed320e9afb2b69e7eb9db722cd15
Gabriel Tamez <gabriel.tamez@gmail.com>
2019-03-27 17:11:28 -0600

    refactored artist page, flipped tshirt design page

--- db/migrate/20190325202251_add_default_value_to_price_cents.rb
+++ db/migrate/20190325202251_add_default_value_to_price_cents.rb
@@ -1,5 +1,5 @@
 class AddDefaultValueToPriceCents < ActiveRecord::Migration[5.2]
   def change
-    change_column :tshirts, :price_cents, :integer, :default => 25
+    add_column :tshirts, :price_cents, :integer, :default => 25
   end
 end
