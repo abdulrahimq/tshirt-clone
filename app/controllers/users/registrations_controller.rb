9372a6eeca5c277e640482623058409ac7906fd7
Zachary Irlbeck <zachCDMX@gmail.com>
2019-03-28 12:34:07 -0600

    fixed the redirecting of the edit user page to the user profile, and added social share buttons on the tshirt show

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -9,4 +9,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def account_update_params
     params.require(:user).permit(:photo, :first_name, :last_name, :email, :password, :password_confirmation, :current_password)
   end
+
+  protected
+
+  # lets not delete this line this time - it redirects the user after update
+
+  def after_update_path_for(resource)
+    user_path(resource)
+  end
 end
