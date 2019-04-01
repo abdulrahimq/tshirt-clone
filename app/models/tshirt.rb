ac00d16607df34c5ad15d5841b7e6e2652ec8beb
Gabriel Tamez <gabriel.tamez@gmail.com>
2019-04-01 15:32:44 -0600

    added facet menu to index page

--- app/models/tshirt.rb
+++ app/models/tshirt.rb
@@ -1,7 +1,5 @@
 class Tshirt < ApplicationRecord
   include AlgoliaSearch
-
-
   algoliasearch do
     attribute :name, :description, :tags
   end
@@ -11,4 +9,16 @@ class Tshirt < ApplicationRecord
   has_many :items
   mount_uploader :photo, PhotoUploader
   validates :photo, presence: true
+
+  def self.tagsArray(limit)
+    dirtyArray = []
+    Tshirt.all.each do |tshirt|
+      if tshirt.tags != nil
+      dirtyArray << tshirt.tags.strip.split(", ")
+      end
+    end
+    cleanArray = dirtyArray.flatten
+    cleanHash = Hash[cleanArray.uniq.map {|v| [v, cleanArray.count(v)] }].sort_by {|k,v| -v }[0...limit.to_i]
+    finalArray = cleanHash.map {|k,v| k }.sort
+  end
 end
