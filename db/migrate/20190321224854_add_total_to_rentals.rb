e72b76d3d2b17890c54ee69a82e4fe09b8eecb0f
Abed Qaddoumi <abdulrahim.qaddoumi@gmail.com>
2019-04-01 22:37:04 -0600

    removed some rental migration files

--- db/migrate/20190321224854_add_total_to_rentals.rb
+++
@@ -1,7 +0,0 @@
-class AddTotalToRentals < ActiveRecord::Migration[5.2]
-  def change
-    add_column :rentals, :total, :integer
-    remove_column :rentals, :content
-    remove_column :rentals, :text
-  end
-end
