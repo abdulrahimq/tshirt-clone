e03cf49d0e6da0543435c0fd4affb5fd48afa246
Abed Qaddoumi <abdulrahim.qaddoumi@gmail.com>
2019-04-01 22:29:38 -0600

    added comments to what can be refactored

--- app/assets/javascripts/new_design.js
+++ app/assets/javascripts/new_design.js
@@ -1,14 +1,17 @@
+// TODO this can be changed to interpolate the string instead of using 3 different cases
+
 function check_color(value) {
   const photo = document.getElementById("imagedest")
+  console.log(imageTag('White.jpg'))
   switch (value) {
-  case 1:
-    photo.src = "https://res.cloudinary.com/dju9xcsec/image/upload/v1553144623/White.jpg"
+  case "White":
+    photo.src = 'https://res.cloudinary.com/dju9xcsec/image/upload/v1553144623/White.jpg'
   break;
-  case 2:
-    photo.src = "https://res.cloudinary.com/dju9xcsec/image/upload/v1553144614/Gray.jpg"
+  case "Grey":
+    photo.src = 'https://res.cloudinary.com/dju9xcsec/image/upload/v1553144614/Gray.jpg'
   break;
-  case 3:
-    photo.src = "https://res.cloudinary.com/dju9xcsec/image/upload/v1553144618/Black.jpg"
+  case "Black":
+    photo.src = 'https://res.cloudinary.com/dju9xcsec/image/upload/v1553144618/Black.jpg'
   break;
   }
 }
