7b65b3b7598c1662f0acc2b929691674c87c968c
Zachary Irlbeck <zachCDMX@gmail.com>
2019-04-03 08:52:02 -0600

    drying out card worked, testing making clickable js function

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,7 @@
 import "bootstrap";
-import 'mapbox-gl/dist/mapbox-gl.css';
-import { initMapbox } from '../plugins/init_mapbox';
-import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
-import { initSearch} from '../plugins/algolia_search'
+// import 'mapbox-gl/dist/mapbox-gl.css';
+// import { initMapbox } from '../plugins/init_mapbox';
+// import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+// import { initSearch} from '../plugins/algolia_search'
 // initSearch();
 
--- app/javascript/plugins/algolia_search.js
+++ app/javascript/plugins/algolia_search.js
@@ -1,19 +1,22 @@
-const searchFunction = (event) => {
-  event.preventDefault();
-  const query = 'new'
-  const client = algoliasearch(ENV['ALGOLIA_APPLICATION_ID'], ENV['ALGOLIA_PUBLIC_API_KEY']);
-  const index = client.initIndex('Tshirt');
-  index.search(query, { hitsPerPage: 10, page: 0}).then(function searchDone(content) {
-    console.log(content);
-  }).catch(function searchFailure(err) {
-    console.error(err);
-  });
-}
+// const searchFunction = (event) => {
+//   event.preventDefault();
+//   const query = 'new'
+//   const client = algoliasearch(ENV['ALGOLIA_APPLICATION_ID'], ENV['ALGOLIA_PUBLIC_API_KEY']);
+//   const index = client.initIndex('Tshirt');
+//   index.search(query, { hitsPerPage: 10, page: 0}).then(function searchDone(content) {
+//     console.log(content);
+//   }).catch(function searchFailure(err) {
+//     console.error(err);
+//   });
+// }
 
 
-const initSearch = () => {
-  const search = document.querySelector('form');
-  search.addEventListener('submit', searchFunction(event));
-}
+// const initSearch = () => {
+//   const search = document.querySelector('form');
+//   search.addEventListener('submit', searchFunction(event));
+// }
 
-export { initSearch };
+// export { initSearch };
+
+
+// tried to implement front end search and failded - ignore code
