7c249cc0d6ced530735f1c88c9abff5f42f3bc12
Zachary Irlbeck <zachCDMX@gmail.com>
2019-04-04 11:32:01 -0600

    everything is working with the drag and drop of interact.js

--- app/javascript/packs/resizable.js
+++ app/javascript/packs/resizable.js
@@ -1,6 +1,5 @@
 const animateResize = (event) => {
   const target = event.target;
-  console.log(target);
   target.style.width = event.rect.width + 'px';
   target.style.height = event.rect.height + 'px';
 }
