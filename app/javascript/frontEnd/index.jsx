659b251e8cbd8a5c2000e4d76517cb54949c9096
Zachary Irlbeck <zachCDMX@gmail.com>
2019-04-26 09:51:37 -0500

    restored index page for react

--- app/javascript/frontEnd/index.jsx
+++ app/javascript/frontEnd/index.jsx
@@ -33,6 +33,40 @@ ReactDOM.render(
     </Provider>,
   document.getElementById('root')
 );
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import { createBrowserHistory as history } from 'history';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import reduxPromise from 'redux-promise';
+import { logger } from 'redux-logger';
 
+
+import Home from './containers/home';
+import TshirtReducer from './reducers/tshirt_reducer';
+import './stylesheets/index.scss';
+
+const root = document.getElementById('root');
+const initialState = { tshirts: JSON.parse(root.dataset.tshirts) };
+console.log(initialState);
+
+const reducers = combineReducers({
+  tshirts: TshirtReducer
+});
+
+const middlewares = applyMiddleware(reduxPromise, logger);
+
+
+ReactDOM.render(
+    <Provider store={createStore(reducers, initialState, middlewares)}>
+      <Router history={history}>
+        <Switch>
+            <Route path="/" component={Home} />
+        </Switch>
+      </Router>
+    </Provider>,
+  document.getElementById('root')
+);
 
 
