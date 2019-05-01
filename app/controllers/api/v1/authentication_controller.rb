e95e70a741f97e6d554e8f6c85735d0300cadee7
Zachary Irlbeck <zachCDMX@gmail.com>
2019-05-01 14:32:12 -0500

    now onsubmit with correct credentials, we recieve our token back and set in local storage, need to make an action to update the state

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -5,8 +5,9 @@ class Api::V1::AuthenticationController < Api::V1::BaseController
 
   def create
     # does something similiar to devise sessions controller
-    user = User.find_by(email: params[:user][:email])
-    if user.valid_password? params[:user][:password]
+    user = User.find_by(email: params[:email])
+    puts user
+    if user.valid_password? params[:password]
       render json: { token: JsonWebToken.encode(sub: user.id )}
     else
       render json: { errors: ["Invalid email or password"]}
--- app/javascript/frontEnd/containers/loginPage.jsx
+++ app/javascript/frontEnd/containers/loginPage.jsx
@@ -4,6 +4,21 @@ import LoginForm from './login';
 class LoginPage extends React.Component {
   submit = values => {
     console.log('submit works',values)
+    const data = {
+      email: values.email,
+      password: values.password
+    }
+    fetch('/api/v1/auth', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    }).then( res => res.json())
+    .then(data => {
+       localStorage.setItem("token", data.token)
+    })
+    .catch(error => console.error('Error:', error));
   }
 
   render() {
