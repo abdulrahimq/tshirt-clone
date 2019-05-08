d1d0cd715dab5adbb4783da2c952a14a5ebcd6b1
Zachary Irlbeck <zachCDMX@gmail.com>
2019-05-08 13:54:11 -0500

    fixed an extra div

--- app/models/tshirt.rb
+++ app/models/tshirt.rb
@@ -5,25 +5,23 @@ class Tshirt < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true, length: { maximum: 150 }
   validates :photo, presence: true
-  include AlgoliaSearch
-  algoliasearch do
-    attribute :name, :description, :tags
-  end
-
   monetize :price_cents
   belongs_to :user
   has_many :items
   mount_uploader :photo, PhotoUploader
 
-  def self.tagsArray(limit)
-    dirtyArray = []
-    Tshirt.all.each do |tshirt|
-      if tshirt.tags != nil
-      dirtyArray << tshirt.tags.strip.split(", ")
-      end
-    end
-    cleanArray = dirtyArray.flatten
-    cleanHash = Hash[cleanArray.uniq.map {|v| [v, cleanArray.count(v)] }].sort_by {|k,v| -v }[0...limit.to_i]
-    finalArray = cleanHash.map {|k,v| k }.sort
+  # def self.tagsArray(limit)
+  #   dirtyArray = []
+  #   Tshirt.all.each do |tshirt|
+  #     if tshirt.tags != nil
+  #     dirtyArray << tshirt.tags.strip.split(", ")
+  #     end
+  #   end
+  #   cleanArray = dirtyArray.flatten
+  #   cleanHash = Hash[cleanArray.uniq.map {|v| [v, cleanArray.count(v)] }].sort_by {|k,v| -v }[0...limit.to_i]
+  #   finalArray = cleanHash.map {|k,v| k }.sort
+  # end
+  def self.tagsArray
+    return ['chic', 'mens', 'women', 'business', 'casual', 'funny', 'social', 'aware']
   end
 end
