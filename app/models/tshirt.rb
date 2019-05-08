3c7c9266bf6749b96af3da5724d380ddc5cd3ece
Zachary Irlbeck <zachCDMX@gmail.com>
2019-05-07 20:06:59 -0500

    removed mention of aloglia search so I can test deploying to AWS

--- app/models/tshirt.rb
+++ app/models/tshirt.rb
@@ -5,11 +5,6 @@ class Tshirt < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true, length: { maximum: 150 }
   validates :photo, presence: true
-  include AlgoliaSearch
-  algoliasearch do
-    attribute :name, :description, :tags
-  end
-
   monetize :price_cents
   belongs_to :user
   has_many :items
